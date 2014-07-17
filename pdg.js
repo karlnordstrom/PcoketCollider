5ae8755af7719d4acd6a61aafad78010c47e0013
Christoph Sterz <christoph.sterz@student.hpi.uni-potsdam.de>
2014-07-17 11:40:13 +0200

    added PDG. Just determine the type of the particle

---
+++ pdg.js
@@ -0,0 +1,8 @@
+.pragma library
+
+// PDG
+//This is the Library to store the Properties of different particles
+var Particles = {
+	Electron:{mass: 1, charge: 1, lifetime:1000},
+	Positron:{mass: 1, charge: -1, lifetime:1000}
+	}
